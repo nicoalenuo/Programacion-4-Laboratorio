f76616f662fac4545fdb7489efb7cf142d3c52e3
nicoalenuo <nicoalenuo@gmail.com>
2022-06-08 11:17:51 -0300

    Update estadia.h

--- include/estadia.h
+++ include/estadia.h
@@ -17,7 +17,7 @@ class estadia{
         Date* fechaSalida;
         int codigo;
     public:
-        estadia(Date,Date,int);
+        estadia(Date,Date*,int);
         ~estadia();
 
         calificacion* getCalificacion(){
