f8171bf62d38a4f2575d2b0b12d44b991aa4c0b3
Kevin Machado <kevinmac@adinet.com.uy>
2022-06-10 17:47:51 -0300

    xd

--- include/ControladorUsuario/ControladorUsuario.h
+++ include/ControladorUsuario/ControladorUsuario.h
@@ -39,22 +39,7 @@ class ControladorUsuario:public IControladorUsuario {
         DTUsuario* devolverDatos();
         map<string,DTHuesped*> obtenerHuespedes();
         void AsignarEmpleadoAHostal(string);
-    map<string,DTUsuario*> obtenerUsuarios(){
-        map<string,DTUsuario*> usuarios={};
-        map<string,DTEmpleado*> datosEmpleados=obtenerEmpleados();
-        map<string,DTHuesped*> datosHuespedes=obtenerHuespedes();
-        map<string,DTEmpleado*>::iterator it1;
-        map<string,DTHuesped*>::iterator it2;
-        for (it1=datosEmpleados.begin() ; it1!=datosEmpleados.end() ; it1++){
-            DTEmpleado* dte= (*it1).second;
-            usuarios.insert(pair<string,DTUsuario*>((*it1).first,dte));
-        }
-        for (it2=datosHuespedes.begin() ; it2!=datosHuespedes.end() ; it2++){
-            DTHuesped* dth = (*it2).second;
-            usuarios.insert(pair<string,DTUsuario*>((*it2).first,dth));
-        }
-        return usuarios;
-    }
+        map<string,DTUsuario*> obtenerUsuarios();
         void liberarUsuario();
         void suscribirEmpleado(string);
         //Cosas que agregue
