0134fbd7835de0cfc564b9b6cd2b954310f1e38b
CamilaAM <88860545+CamilaAM@users.noreply.github.com>
2022-06-11 20:21:09 -0300

    d

--- src/habitacion.cpp
+++ src/habitacion.cpp
@@ -1,4 +1,10 @@
 #include "../include/habitacion.h"
+
+habitacion::habitacion(int numero, float precio, int capacidad){
+        (*this).numero = numero;
+        (*this).precio = precio;
+        (*this).capacidad = capacidad;
+}
 void habitacion::agregarReservaAMap(reserva * r){
         (*this).reservas.insert(pair<int,reserva*>((*r).getCodigo(),r));
 }
